f2742d651bb9e3968352431c98896c906eaf6f54
moradi25 <onlinemorad7@gmail.com>
2025-02-11 17:32:10 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+/* script.js */
+document.getElementById("registrationForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Registration submitted successfully!");
+});
